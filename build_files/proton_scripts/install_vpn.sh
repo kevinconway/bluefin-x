c95a47931fa205d66649599df81f5a68fc7806f3
Kevin Conway <kevin@conway0.com>
2026-02-28 17:40:22 -0600

    Disable scripts for protonvpn to avoid systemd dep

--- build_files/proton_scripts/install_vpn.sh
+++ build_files/proton_scripts/install_vpn.sh
@@ -5,4 +5,4 @@ set -ouex pipefail
 ## See https://protonvpn.com/support/official-linux-vpn-fedora for updates
 wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm"
 dnf5 install -y ./protonvpn-stable-release-1.0.3-1.noarch.rpm
-dnf5 check-update --refresh proton-vpn-gnome-desktop && dnf5 install -y proton-vpn-gnome-desktop
+dnf5 check-update --refresh proton-vpn-gnome-desktop && dnf5 install -y --setopt=tsflags=noscripts proton-vpn-gnome-desktop
